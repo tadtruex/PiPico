f1e78ef6095880a34cdda73c2873210524b46fb0
Tad Truex <tad@dartmouth.edu>
2023-09-20 14:12:25 -0400

    Start playing with interrupt handler

--- AirCar/aircar/aircar.c
+++ AirCar/aircar/aircar.c
@@ -1,6 +1,30 @@
 
 #include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include <stdint.h>
+
+void gpioInt( uint gpio, uint32_t eventMask );
+
 
 int main() {
-    while (1);
+
+	stdio_init_all();
+
+	gpio_set_function(16, GPIO_FUNC_SIO );
+	gpio_set_function(17, GPIO_FUNC_SIO );
+
+	gpio_pull_up(16);
+    gpio_pull_up(17);
+
+    gpio_set_irq_callback(gpioInt);
+    gpio_set_irq_enabled(16, GPIO_IRQ_EDGE_RISE|GPIO_IRQ_EDGE_FALL, true);
+
+    while(1);
+}
+
+
+
+
+void gpioInt( uint gpio, uint32_t eventMask ){
+	while(1);
 }
